838df45394b1b9ee7c70a5b8c946c1b6287a1c0c
juanig98 <juanigalarza98@gmail.com>
2023-04-18 08:37:06 -0300

    change: veconsumer

--- src/services/ve-consumer.service.ts
+++ src/services/ve-consumer.service.ts
@@ -1,5 +1,5 @@
 import { EndpointsEnum } from "../enums";
-import { IVEConsumerPortSoap, VEConsumerPortTypes } from "../soap/interfaces/VEConsumerService/VEConsumerPort";
+import { IVEConsumerPortSoap, IconsultarComunicacionesOutput, IconsultarEstadosOutput, IconsultarSistemasPublicadoresOutput, IconsumirComunicacionOutput, VEConsumerPortTypes } from "../soap/interfaces/VEConsumerService/VEConsumerPort";
 import { ServiceNamesEnum } from "../soap/service-names.enum";
 import { WsdlPathEnum } from "../soap/wsdl-path.enum";
 import { AfipContext } from "../types";
@@ -17,6 +17,11 @@ export class VEConsumerService extends AfipService<IVEConsumerPortSoap> {
         });
     }
 
+    private async authRequest(): Promise<VEConsumerPortTypes.IauthRequest> {
+        const { Auth } = await this.getAuthTokens();
+        return { cuitRepresentada: Auth.Cuit, token: Auth.Token, sign: Auth.Sign }
+    }
+
     /** Document: https://www.afip.gob.ar/ws/WSCDCV1/WSCDC_manual_desarrollador_v.2.pdf  */
 
     /**
@@ -32,38 +37,66 @@ export class VEConsumerService extends AfipService<IVEConsumerPortSoap> {
         return output;
     }
 
-    private async authRequest(): Promise<VEConsumerPortTypes.IauthRequest> {
-        const { Auth } = await this.getAuthTokens();
-        return { cuitRepresentada: Auth.Cuit, token: Auth.Token, sign: Auth.Sign }
-    }
-
-    async consultarComunicaciones(filter: VEConsumerPortTypes.Ifilter) {
+    /**
+     * El Sistema sólo mostrará las Comunicaciones destinadas a Contribuyentes filtrando las Comunicaciones realizadas por Sistemas que se encuentren 
+     * relacionados con el Subservicio “e-ventanilla-int-2”. Para identificar los Sistemas publicadores permitidos para exponer consultas de Comunicaciones 
+     * vía Web Services podrá invocar el mensaje ConsultarSistemas (ver seccion Consultar Sistemas). El Sistema sólo consultará Comunicaciones con una 
+     * antigüedad máxima de 360 días a la fecha de la invocación evitando así generar consultas de alto costo. El Sistema filtrará aquellas Comunicaciones 
+     * no pertenecientes a la Cuit indicada en el elemento AuthRequest.cuitRepresentada. Por razones de performance el Sistema devolverá un máximo de 
+     * Comunicaciones por respuesta, permitiendo obtener todos los resultados realizando varias llamadas al Webservice. Para esto el Sistema cliente deberá 
+     * implementar un algoritmo de Paginación (http://ui-patterns.com/patterns/Pagination).    
+     * 
+     * @param {VEConsumerPortTypes.Ifilter} filter Filtro sobre paginación
+     * @returns {Promise<IconsultarComunicacionesOutput>} Resultado de la consulta 
+     */
+    async consultarComunicaciones(filter: VEConsumerPortTypes.Ifilter): Promise<IconsultarComunicacionesOutput> {
         const client = await this.getClient();
         const authRequest = await this.authRequest();
         const [output] = await client.consultarComunicacionesAsync({ authRequest, filter });
         return output;
     }
 
-    async consultarSistemasPublicadores(idSistemaPublicador: number) {
+    /**
+     * Consulta sobre sistemas publicadores 
+     * 
+     * @param {number} idSistemaPublicador Id del sistema publicador
+     * @returns {Promise<IconsultarSistemasPublicadoresOutput>} resultado
+     */
+    async consultarSistemasPublicadores(idSistemaPublicador: number): Promise<IconsultarSistemasPublicadoresOutput> {
         const client = await this.getClient();
         const authRequest = await this.authRequest();
         const [output] = await client.consultarSistemasPublicadoresAsync({ authRequest, idSistemaPublicador });
         return output;
     }
 
-    async consumirComunicacion(idComunicacion: number, incluirAdjuntos: boolean) {
+    /**
+     * El Sistema sólo recupera Comunicaciones que cumplan con las siguientes restricciones:
+     * - La Comunicación debe ser realizada por un Sistema publicador que no se encuentre asociado al servicio “e-ventanilla-int-2” es decir a Comunicaciones 
+     * internas de AFIP. Para identificar los Sistemas publicadores permitidos para exponer y consumir Comunicaciones vía Web Services puede ver más 
+     * información del uso del mensaje ConsultarSistemas (ver seccion “Consultar Sistemas”).
+     * - La Comunicación debe tener como máxima antigüedad 360 días.
+     * - La Comunicación solicitada debe pertenecer a la Cuit indicada en AuthRequest.cuitRepresentada.
+     * 
+     * @param {number} idComunicacion Id de la Comunicación
+     * @param {number} incluirAdjuntos Indica si deben incluirse los Adjuntos vía MTOM (Ver el apéndice Documentos Relacionados) en la respuesta
+     * @returns {Promise<IconsumirComunicacionOutput>} resultado
+    */
+    async consumirComunicacion(idComunicacion: number, incluirAdjuntos: boolean): Promise<IconsumirComunicacionOutput> {
         const client = await this.getClient();
         const authRequest = await this.authRequest();
         const [output] = await client.consumirComunicacionAsync({ authRequest, idComunicacion, incluirAdjuntos });
         return output;
     }
 
-    async consultarEstados() {
+    /**
+     * Consulta de estados
+     * 
+     * @returns {Promise<IconsultarEstadosOutput>}
+     */
+    async consultarEstados(): Promise<IconsultarEstadosOutput> {
         const client = await this.getClient();
         const authRequest = await this.authRequest();
         const [output] = await client.consultarEstadosAsync({ authRequest });
         return output;
     }
-
-
 }
--- src/soap/interfaces/VEConsumerService/VEConsumerPort.ts
+++ src/soap/interfaces/VEConsumerService/VEConsumerPort.ts
@@ -1,4 +1,4 @@
-import { Client} from "soap";
+import { Client } from "soap";
 import { SoapAsyncFunc } from "../../../types";
 
 /* tslint:disable:max-line-length no-empty-interface */
@@ -6,7 +6,26 @@ export interface IconsultarComunicacionesInput {
     authRequest: VEConsumerPortTypes.IauthRequest;
     filter: VEConsumerPortTypes.Ifilter;
 }
-
+/**
+ * ***RespuestaPaginada***
+ * - **pagina** *(number)*: Página recuperada.
+ * - **totalPaginas** *(number)*: Total de páginas requeridas para obtener los resultados completos de la consulta solicitada.
+ * - **itemsPorPagina** *(number)*: Cantidad de resultados que devuelve el Sistema por página. El valor máximo permitido es de 500 resultados, pudiendo variar en el tiempo según el rendimiento del Sistema. Debe utilizar “itemsPorPagina” como variable para paginar si totalItems es mayor a itemsPorPagina. Por otro lado el elemento “totalPaginas” le permite controlar cuantas páginas debe recuperar para obtener el total. Se deben realizar tantos Requests Soap como páginas se requieren recuperar.
+ * - **totalItems** *(number)*: Cantidad de resultados encontrados para la consulta.
+ *
+ * ***ItemContainer***
+ * - **idComunicacion** *(number)* Id de la Comunicación.
+ * - **cuitDestinatario** *(number)*: Cuit del destinatario de la Comunicación.
+ * - **fechaPublicacion** *(string)*: Fecha en que se realizo la publicación.
+ * - **fechaVencimiento** *(string)*: Fecha que vence la Comunicación.
+ * - **sistemaPublicador** *(int)*: Sistema que realizó la publicación.
+ * - **sistemaPublicadorDesc** *(string)*: Nombre del Sistema Publicador Si Mis facilidades.
+ * - **estado** *(int)*: Estado de la Comunicación (1, 2). 1=No leida, 2=Leida.
+ * - **estadoDesc** *(string)*: Descripción del estado de la Comunicación.
+ * - **asunto** *(string)*: Asunto de la Comunicación. Solo si la Comunicación no tiene un “asunto” asociado se devuelven los primeros 50 caracteres del mensaje.
+ * - **prioridad** *(int)*: Priorización dada por el Publicador. 1=Alta, 2=Media, 3=Baja.
+ * - **tieneAdjunto** *(boolean)*: Indica si la Comunicación tiene un adjunto asociado.
+ */
 export interface IconsultarComunicacionesOutput {
     RespuestaPaginada: VEConsumerPortTypes.IRespuestaPaginada;
 }
@@ -17,11 +36,21 @@ export interface IconsultarSistemasPublicadoresInput {
     idSistemaPublicador: number;
 }
 
+/**
+ * ***Sistemas <Sistema>***
+ * - **id** *(Integer)*: Identificación del Sistema.
+ * - **descripcion** *(String)*: Nombre del Sistema.
+ * - **certCN** *(String)*: Common Name para autenticación por SSL cruzado 
+ * 
+ * ***Subservicios<Subservicio>***
+ * - **nombre** *(String)*: Nombre del servicio habilitado
+ * - **descripcion** *(String)*: Descripción
+ */
 export interface IconsultarSistemasPublicadoresOutput {
     Sistemas: VEConsumerPortTypes.ISistemas;
 }
 
-export interface IdummyInput {}
+export interface IdummyInput { }
 
 export interface IdummyOutput {
     DummyResult: VEConsumerPortTypes.IDummyResult;
@@ -34,7 +63,22 @@ export interface IconsumirComunicacionInput {
     /** http://ve.tecno.afip.gov.ar/domain/service/ws/types#xs:boolean(undefined) */
     incluirAdjuntos: boolean;
 }
-
+/**
+ * - ***Comunicacion*** {
+ * - - **idComunicacion** *(number)*: Id de la Comunicación Si 123456789
+ * - - **cuitDestinatario** *(number)*: Cuit del destinatario de la Comunicación.
+ * - - **fechaPublicacion** *(string)*: Fecha en que se realizo la publicación.
+ * - - **fechaVencimiento** *(string)*: Fecha en que vence la Comunicación.
+ * - - **sistemaPublicador** *(number)*: Sistema que realizó la publicación.
+ * - - **sistemaPublicadorDesc** *(string)*: Nombre del Sistema Publicador.
+ * - - **estado** *(number)*: Estado de la Comunicación (1, 2).
+ * - - **estadoDesc** *(string)*: Descripción del estado de la comunicación (No leida, Leida).
+ * - - **asunto** *(string)*: Asunto de la Comunicación.
+ * - - **Prioridad** *(number)*: Priorizacion dada por el Publicador.
+ * - - **tieneAdjunto** *(Boolean)*: Indica si la Comunicación tiene un adjunto asociado.
+ * - - **Mensaje** *(string)*: Mensaje contenido en la Comunicación 
+ * }
+ */
 export interface IconsumirComunicacionOutput {
     Comunicacion: VEConsumerPortTypes.IComunicacion;
 }
@@ -43,16 +87,20 @@ export interface IconsultarEstadosInput {
     authRequest: VEConsumerPortTypes.IauthRequest;
 }
 
+/**
+ * - **id** *(number)*: Identificación del Estado
+ * - **descripcion** *(string)*: Descripción del Estado de la Comunicación 
+ */
 export interface IconsultarEstadosOutput {
     Estados: VEConsumerPortTypes.IEstados;
 }
 
 export interface IVEConsumerPortSoap extends Client {
-    consultarComunicaciones: (input: IconsultarComunicacionesInput, cb: (err: any | null, result: IconsultarComunicacionesOutput, raw: string,  soapHeader: {[k: string]: any; }) => any, options?: any, extraHeaders?: any) => void;
-    consultarSistemasPublicadores: (input: IconsultarSistemasPublicadoresInput, cb: (err: any | null, result: IconsultarSistemasPublicadoresOutput, raw: string,  soapHeader: {[k: string]: any; }) => any, options?: any, extraHeaders?: any) => void;
-    dummy: (input: IdummyInput, cb: (err: any | null, result: IdummyOutput, raw: string,  soapHeader: {[k: string]: any; }) => any, options?: any, extraHeaders?: any) => void;
-    consumirComunicacion: (input: IconsumirComunicacionInput, cb: (err: any | null, result: IconsumirComunicacionOutput, raw: string,  soapHeader: {[k: string]: any; }) => any, options?: any, extraHeaders?: any) => void;
-    consultarEstados: (input: IconsultarEstadosInput, cb: (err: any | null, result: IconsultarEstadosOutput, raw: string,  soapHeader: {[k: string]: any; }) => any, options?: any, extraHeaders?: any) => void;
+    consultarComunicaciones: (input: IconsultarComunicacionesInput, cb: (err: any | null, result: IconsultarComunicacionesOutput, raw: string, soapHeader: { [k: string]: any; }) => any, options?: any, extraHeaders?: any) => void;
+    consultarSistemasPublicadores: (input: IconsultarSistemasPublicadoresInput, cb: (err: any | null, result: IconsultarSistemasPublicadoresOutput, raw: string, soapHeader: { [k: string]: any; }) => any, options?: any, extraHeaders?: any) => void;
+    dummy: (input: IdummyInput, cb: (err: any | null, result: IdummyOutput, raw: string, soapHeader: { [k: string]: any; }) => any, options?: any, extraHeaders?: any) => void;
+    consumirComunicacion: (input: IconsumirComunicacionInput, cb: (err: any | null, result: IconsumirComunicacionOutput, raw: string, soapHeader: { [k: string]: any; }) => any, options?: any, extraHeaders?: any) => void;
+    consultarEstados: (input: IconsultarEstadosInput, cb: (err: any | null, result: IconsultarEstadosOutput, raw: string, soapHeader: { [k: string]: any; }) => any, options?: any, extraHeaders?: any) => void;
     consultarComunicacionesAsync: SoapAsyncFunc<IconsultarComunicacionesInput, IconsultarComunicacionesOutput>,
     consultarSistemasPublicadoresAsync: SoapAsyncFunc<IconsultarSistemasPublicadoresInput, IconsultarSistemasPublicadoresOutput>,
     dummyAsync: SoapAsyncFunc<IdummyInput, IdummyOutput>,
@@ -69,6 +117,22 @@ export namespace VEConsumerPortTypes {
         /** http://core.tecno.afip.gov.ar/model/ws/types#xs:long(undefined) */
         cuitRepresentada: number;
     }
+    /**
+     * Ifilter. 
+     * Atributos:
+     * 
+     * - Estado *(number)* (1): Estado de la Comunicación.
+     * - fechaDesde *(string)*: Fecha limite inferior de las Comunicaciones a buscar. No debe ser inferior a 360 días desde la fecha actual. Se debe utilizar el formato de fecha “yyyy-MM-dd”.
+     * - fechaHasta *(string)*: Fecha limite superior de las Comunicaciones a buscar. Se debe utilizar el formato de fecha “yyyy-MM-dd”. Debe ser superior a fechaDesde.
+     * - comunicacionIdDesde *(number)*: Nro de id de la Comunicación. Rango inferior. La Comunicación no debe ser con fecha de publicación inferior a 360 días desde la fecha actual.
+     * - comunicacionIdHasta *(number)*: Nro de id de la Comunicación. Rango superior. La Comunicación no debe ser con fecha de publicación inferior a 360 días desde la fecha actual.
+     * - tieneAdjunto *(boolean)*: Devuelve solo las Comunicaciones que tienen asociado un archivo adjunto.
+     * - sistemaPublicadorId *(number)* (3): Identificación del sistema publicador. Ver flujo “Consultar Sistemas Publicadores” 
+     * - pagina *(number)*: Numero de pagina a obtener. 
+     * - resultadosPorPagina *(number)*: Limite de filas a recuperar por consulta 
+     * - Referencia1 *(string)* (255): Valor seteado por el usuario/sistema al momento publicar la comunicación con el objeto de distinguirla o identificarla. 
+     * - Referencia2 *(string)* (255): Valor seteado por el usuario/sistema al momento de realizar la publicación de la comunicación con el objeto 
+     */
     export interface Ifilter {
         /** http://ve.tecno.afip.gov.ar/domain/service/ws/types#xs:int(undefined) */
         estado: number;
